abcf4726e9146786c4347da4baf78d886cf2f2cb
PILLAI <prajual.pillai@nissanmotor.com>
2025-05-28 17:08:17 +0530

    Reflection chain now in working condition

--- chains.py
+++ chains.py
@@ -11,6 +11,10 @@ reflection_prompt = ChatPromptTemplate.from_messages(
             "system",
             "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user."
             "Always provide detailed recommendations, including requests for length, virality, style, etc"
+            "Do not just say that the tweet is good or bad, give detailed critique"
+            "Do not ask for newer tweets, try to improve upon the existing tweet even after the initial improvements are made"
+            "Do not give a review of why the changes are impactful"
+            "If no further changes are present, just tell the user that the tweet is good enough"
         ),
         MessagesPlaceholder(variable_name="messages")
     ])
@@ -22,15 +26,20 @@ generation_prompt = ChatPromptTemplate.from_messages(
             "You are a twitter techie influencer assistant, tasked with writing excellent twitter posts"
             "Generate the best twitter post possible as per the user's request"
             "If the user provides a critique of the post, consider it and rectify the mistakes made in the previous attempt."
+            "Given the critique only give the modified text based on the critique, don't include extra conversations like check it out and other formalities"
+            "Always rewrite the review according to the critique given"
+            "Do not just say that you'll keep the recommendation in mind for future tweets, re-write the original tweet according to the cumulative critique every time"
+            "Do not give ideas on how to improve, simply improve the tweet"
         ),
         MessagesPlaceholder(variable_name="messages")
     ]
 )
 
 # Initializing chains
-llm = ChatGroq(api_key=os.environ["GROQ_API_KEY"], model="llama3-8b-8192") # distill-whisper-large-v3, deepseek-r1-distill-llama-70b
+gen_llm = ChatGroq(api_key=os.environ["GROQ_API_KEY"], model="deepseek-r1-distill-llama-70b") # llama3-8b-8192
+ref_llm = ChatGroq(api_key=os.environ["GROQ_API_KEY"], model="gemma2-9b-it") # distill-whisper-large-v3, deepseek-r1-distill-llama-70b
 
-generate_chain = generation_prompt | llm
-reflection_chain = reflection_prompt | llm
+generate_chain = generation_prompt | gen_llm
+reflection_chain = reflection_prompt | ref_llm
 
 
--- main.py
+++ main.py
@@ -6,32 +6,41 @@ from langchain_core.messages import BaseMessage, HumanMessage
 from langgraph.graph import END, MessageGraph
 from chains import reflection_chain, generate_chain
 import time
-reflect = "reflect"
-generate = "generate"
+
 
 def generation_node(state: Sequence[BaseMessage]):
-    return generate_chain.invoke({"messages": state}).content
+    gen_response = generate_chain.invoke({"messages": state})
+    print("gen response: ", gen_response)
+    print("state: ", state)
+    return gen_response
 
 def reflection_node(messages: Sequence[BaseMessage])->List[BaseMessage]:
-    res = reflection_chain.invoke({"messages": messages})
     
+    res = reflection_chain.invoke({"messages": messages})
+    print("ref: ", res)
+    print("ref state: ", messages)
     return [HumanMessage(content=res.content)]
 
 
-builder = MessageGraph()
-builder.add_node(generate, generation_node)
-builder.add_node(reflect, reflection_node)
-builder.set_entry_point(generate)
-def should_continue(state:Sequence[BaseMessage]):
-    if len(state)>6:
-        return END
-    else:
-        return reflect
-builder.add_conditional_edges(generate, should_continue)
-builder.add_edge(reflect, generate)
+def build_graph():
 
-graph = builder.compile()
-print(graph.get_graph().print_ascii())
+    reflect = "reflect"
+    generate = "generate"
+    builder = MessageGraph()
+    builder.add_node(generate, generation_node)
+    builder.add_node(reflect, reflection_node)
+    builder.set_entry_point(generate)
+    def should_continue(state:Sequence[BaseMessage]):
+        if len(state)>6:
+            return END
+        else:
+            return reflect
+    builder.add_conditional_edges(generate, should_continue)
+    builder.add_edge(reflect, generate)
+
+    graph = builder.compile()
+    return graph
+# print(graph.get_graph().print_ascii())
 if __name__=="__main__":
     # print(os.environ["GROQ_API_KEY"])
 
@@ -44,5 +53,6 @@ if __name__=="__main__":
             Made a video covering their newest blog post
 
                                   """)
+    graph = build_graph()
     response = graph.invoke(input=inputs)
 
